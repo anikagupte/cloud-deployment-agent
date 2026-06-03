431c88fb734ffdceacf90cf3762da0ec9a647551
Anika <88386880+anikagupte@users.noreply.github.com>
2026-06-03 17:45:34 +0100

    created calculate_scores()

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def collect_inputs():
 
     valid = False
     while not valid:
-        security = input('please enter your security requirements between 1 and 10: ')
+        security = input('please enter your security requirements between 1 and 10, or hit ENTER to skip: ')
         if security == "":
             security = None
             valid = True
@@ -41,6 +41,7 @@ def collect_inputs():
             try:
                 security = int(security)
                 if security >= 0 and security <= 10:
+                    security = security / 10 # normalise score
                     valid = True
             except ValueError:
                 pass
@@ -58,7 +59,7 @@ def collect_inputs():
 
     valid = False
     while not valid:
-        growth = input('please enter your growth multiplier expectation for the next five years: ')
+        growth = input('please enter your growth multiplier expectation for the next five years, or hit ENTER to skip: ')
         if growth == "":
             growth = None
             valid = True
@@ -77,4 +78,51 @@ def collect_inputs():
             valid = True
 
     return {"budget": budget, "storage": storage, "expertise": expertise, "security": security, "sustainability": sustainability, "top_priority": top_priority, "growth": growth}
+
+def calculate_scores(inputs):
+    """
+    calculate scores using weightings
+    check actual docstring formatting
+    """
+    cost_score = 1.0
+    scalability_score = 1.0
+    sustainability_score = 1.0
+    geographic_coverage = 1.0
+    security_score = 1.0
+    complexity_score = 1.0
+    customisability_score = 1.0
+    management_overhead = 1.0
+
+    if inputs["top_priority"] == "cost":
+        cost_score *= 2
+    elif inputs["top_priority"] == "security":
+        security_score *= 2
+    elif inputs["top_priority"] == "sustainability":
+        sustainability_score *= 2
+    elif inputs["top_priority"] == "scalability":
+        scalability_score *= 2
+        
+    sustainability_score *= inputs["sustainability"]
+
+    if inputs["security"] is not None:
+        security_score *= inputs["security"]
+
+    if inputs["expertise"] is not None:
+        if inputs["expertise"] == "none":
+            management_overhead *= 2
+            complexity_score *= 2
+        elif inputs["expertise"] == "basic":
+            management_overhead *= 1.75
+            customisability_score *= 1.25
+            complexity_score *= 1.75
+        elif inputs["expertise"] == "moderate":
+            management_overhead *= 1.5
+            customisability_score *= 1.5
+            complexity_score *= 1.5
+        elif inputs["expertise"] == "high":
+            management_overhead *= 1.25
+            customisability_score *= 1.75
+            complexity_score *= 1.25
+        else:
+            customisability_score *= 2
 
